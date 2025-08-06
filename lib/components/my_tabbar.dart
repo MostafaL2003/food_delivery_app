fb7ada9e08ab794fbb1f3b26b8bbec5903b9ef1f
Mostafa mamdouh <MostafaW2003@outlook.com>
2025-08-06 20:02:45 +0300

    Finish Tabbar

---
+++ lib/components/my_tabbar.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class MyTabbar extends StatelessWidget {
+  const MyTabbar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
